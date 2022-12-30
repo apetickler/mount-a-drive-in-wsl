49c801dea76999181061e3c0cc16b6beb7e8554f
Patrick <apetickler@gmail.com>
2022-12-30 13:51:23 -0800

    Wrote an error message
    I anticipate that failing to isolate the hard drive’s device ID is likely to be a common problem, and the mount command’s error message in that case probably won’t be very helpful. So we want to intercept that case earlier in the process and provide our own error instead.

--- step-2_mount-the-drive.ps1
+++ step-2_mount-the-drive.ps1
@@ -7,11 +7,20 @@ $target_device_id = GET-CimInstance -query "SELECT * from Win32_DiskDrive" | Whe
 # ======
 
 # This is purely cosmetic and can be whatever you please.
-$big_text = "**TITLE**"
+$big_text = "LET’S MOUNT A DRIVE"
 
 # ======
 
-function Write-Output-Centered {
+# And here’s the udev rule that mounts the drive once Linux sees it.
+# Notice that it assumes we’re mounting only one partition. If you’ve
+# got anything more complex going on, you might want to make the rule
+# execute a script.
+#
+# ACTION=="add", SUBSYSTEM=="block", ATTRS{model}=="WDC WD60EZAZ-00S", RUN+="/bin/mount -t btrfs -o noexec,rw /dev/%k1 /mnt/butter"
+
+# ======
+
+function Write-CenteredOutput {
     # I stole this function from
     # https://stackoverflow.com/questions/48621267/is-there-a-way-to-center-text-in-powershell
     param($message)
@@ -27,30 +36,15 @@ function Write-Output-Centered {
         )
 }
 
-function Banner {
+function Write-Banner {
     param($message)
     $border = ("=" * $message.Length)
     foreach ($line in $border, $message, $border) {
-        Write-Output-Centered $line
+        Write-CenteredOutput $line
     }
 }
 
-# ======
-
-# ACT I: First we declare our intentions.
-Write-Output "`n"
-Banner "$big_text" | wsl.exe lolcat
-Write-Output "`n"
-Write-Output-Centered "Making the device $target available to the Linux subsystem..." | wsl.exe lolcat
-Write-Output "`n"
-
-# ACT II: Then we do the thing.
-[array] $mount_command_output = (wsl.exe --mount "$target_device_id" --bare)
-
-if ($LASTEXITCODE -ne 0) {
-    # Looks like something went wrong.
-    # We’d better be loud about it.
-    $problem = "yup"
+function Write-ErrorBanner {
     $error_preamble = "UH OH!"
     while ($error_preamble.Length -lt $Host.UI.RawUI.BufferSize.Width) {
         $error_preamble = $error_preamble + " // " + $error_preamble
@@ -58,25 +52,65 @@ if ($LASTEXITCODE -ne 0) {
     $error_preamble = $error_preamble.Substring(0, $Host.UI.RawUI.BufferSize.Width)
     Write-Host $error_preamble -ForegroundColor White -BackgroundColor DarkRed
     Write-Output "`n"
-    Write-Output-Centered "wsl.exe --mount returned the following error:" | wsl.exe lolcat
+}
+
+# ======
+
+# ACT I: First we declare our intentions.
+
+Write-Output "`n"
+Write-Banner "$big_text" | wsl.exe lolcat
+Write-Output "`n"
+Write-CenteredOutput "Making the device $target available to the Linux subsystem..." | wsl.exe lolcat
+Write-Output "`n"
+
+# ACT II: Then we do the thing.
+
+# If we failed to isolate a device ID, there’s nothing to mount.
+if ($target_device_id.Length -eq 0) {
+    Write-ErrorBanner
+    Write-CenteredOutput "Failed to isolate a device ID." | wsl.exe lolcat
+
+    foreach ($line in `
+    "Double-check the value of the variable `"`$target`"", `
+    "at the beginning of this script, or try massaging", `
+    "the pipeline that defines `"`$target_device_id`".") {
+        Write-CenteredOutput $line
+    }
+
+    Write-Output "`n"
+    Read-Host -Prompt (Write-CenteredOutput "Press any key to exit")
+    return
+}
+
+[array] $mount_command_output = (wsl.exe --mount "$target_device_id" --bare)
+
+if ($LASTEXITCODE -ne 0) {
+    # Looks like something went wrong.
+    # We’d better be loud about it.
+    $problem = "yup"
+    Write-ErrorBanner
+    Write-CenteredOutput "wsl.exe --mount returned the following error:"
 }
 else {
     # Looks like the operation was a success.
     $problem = "nope"
 }
 
-# ACT III: Here’s the output from the mount command.
+# ACT III: We assess the results.
+
+# Here’s the output from the mount command.
 foreach ($line in $mount_command_output) {
     # The replace argument here compensates for an
     # annoying encoding mismatch between WSL and PowerShell.
-    Write-Output-Centered ($line -replace "`0","") | wsl.exe lolcat
+    Write-CenteredOutput ($line -replace "`0","") | wsl.exe lolcat
 }
 # If the mount seems to have worked, we exit automatically.
 if ($problem -eq "nope") {
-    Write-Output-Centered "This message will self-destruct." | wsl.exe lolcat
+    Write-CenteredOutput "This message will self-destruct." | wsl.exe lolcat
     Start-Sleep -Seconds 5
 }
 # If not, we want to know about it.
 if ($problem -eq "yup") {
-    Read-Host -Prompt (Write-Output-Centered "Press any key and despair")
+    Read-Host -Prompt (Write-CenteredOutput "Press any key and despair")
 }
