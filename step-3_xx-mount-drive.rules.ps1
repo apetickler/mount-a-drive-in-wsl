fdd0c0b0735916d28b0cb882d18ccb4437cb42cc
Patrick <apetickler@gmail.com>
2022-12-30 13:41:20 -0800

    Delete step-3_xx-mount-drive.rules.ps1
    I didn’t notice that extraneous, incorrect filename extension before I uploaded this.

--- step-3_xx-mount-drive.rules.ps1
+++
@@ -1,6 +0,0 @@
-# Here’s the udev rule that mounts the drive once Linux sees it.
-# Notice that it assumes we’re mounting only one partition. If you’ve
-# got anything more complex going on, you might want to make the rule
-# execute a script.
-
-ACTION=="add", SUBSYSTEM=="block", ATTRS{model}=="**MODEL**", RUN+="/bin/mount -t **FILESYSTEM** -o **OPTIONS** /dev/%k1 /**MOUNT-POINT**"
